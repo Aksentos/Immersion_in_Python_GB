dd14627fab3dd2e84ad72a4bc00cce3153cedb1a
Aksentos <108864454+Aksentos@users.noreply.github.com>
2024-04-07 17:17:58 +0300

    Update hw_11.py

--- Home_work/hw_11.py
+++ Home_work/hw_11.py
@@ -201,3 +201,71 @@ class Rectangle:
 
     def __repr__(self) -> str:
         return f"Rectangle({self.width}, {self.height})"
+
+
+'''Задача о матричных операциях
+Разработать класс Matrix, представляющий матрицу и обеспечивающий базовые операции с матрицами.
+
+Атрибуты класса:
+rows (int): Количество строк в матрице.
+cols (int): Количество столбцов в матрице.
+data (list): Двумерный список, содержащий элементы матрицы.
+
+__init__(self, rows, cols): Конструктор класса, который инициализирует атрибуты rows и cols, а 
+также создает двумерный список data размером rows x cols и заполняет его нулями.
+
+__str__(self): Метод, возвращающий строковое представление матрицы. Возвращаемая строка 
+представляет матрицу, где элементы разделены пробелами, а строки разделены символами новой строки. 
+
+__repr__(self): Метод, возвращающий строковое представление объекта, которое может быть 
+использовано для создания нового объекта того же класса с такими же размерами и данными.
+
+__eq__(self, other): Метод, определяющий операцию "равно" для двух матриц. Сравнивает 
+две матрицы и возвращает True, если они имеют одинаковое количество строк и столбцов, 
+а также все элементы равны. Иначе возвращает False.
+
+__add__(self, other): Метод, определяющий операцию сложения двух матриц. Проверяет, 
+что обе матрицы имеют одинаковые размеры (количество строк и столбцов). Если размеры 
+совпадают, создает новую матрицу, где каждый элемент равен сумме соответствующих 
+элементов входных матриц.
+
+__mul__(self, other): Метод, определяющий операцию умножения двух матриц. Проверяет, 
+что количество столбцов в первой матрице равно количеству строк во второй матрице. 
+Если условие выполняется, создает новую матрицу, где элемент на позиции [i][j] 
+равен сумме произведений элементов соответствующей строки из первой матрицы и столбца 
+из второй матрицы.
+'''
+
+class Matrix:
+    def __init__(self, rows: int, cols: int):
+        self.rows = rows
+        self.cols = cols
+        self.data = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    def __str__(self):
+        return "\n".join([" ".join(map(str, i)) for i in self.data])
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.rows}, {self.cols})"
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __add__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            new_matrix = Matrix(self.rows, self.cols)
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    new_matrix.data[i][j] = self.data[i][j] + other.data[i][j]
+            return new_matrix
+        raise ValueError('Размерность матриц разная')
+
+    def __mul__(self, other):
+        if self.cols == other.rows:
+            new_matrix = Matrix(self.rows, self.cols)
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    for k in range(self.cols):
+                        new_matrix.data[i][j] += self.data[i][k] * other.data[k][j]
+            return new_matrix
+        raise ValueError('Размерность матриц разная')
