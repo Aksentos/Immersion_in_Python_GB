48ff6e60555991ddd2b0e8b0c5bd6578397b6fa2
Aksentos <108864454+Aksentos@users.noreply.github.com>
2024-04-14 10:28:20 +0300

    Update hw_13.py

--- Home_work/hw_13.py
+++ Home_work/hw_13.py
@@ -119,3 +119,96 @@ r.height = -3
 Ожидаемый ответ:
 __main__.NegativeValueError: Высота должна быть положительной, а не -3
 """
+
+'''Обработка исключений в Archive
+Допишите в вашу задачу Archive обработку исключений.
+Добавьте исключение в ваш код InvalidTextError, которые будет вызываться, 
+когда текст не является строкой или является пустой строкой.
+Текст ошибки: Invalid text: {введенный текст}. Text should be a non-empty string.'
+И InvalidNumberError, которое будет вызываться, если число не является 
+положительным целым числом или числом с плавающей запятой.
+Текст ошибки: 'Invalid number: {введенное число}. Number should be a positive 
+integer or float.'
+'''
+from typing import Union
+
+class Archive:
+    """
+    Класс, представляющий архив текстовых и числовых записей.
+
+    Атрибуты:
+    - archive_text (list): список архивированных текстовых записей.
+    - archive_number (list): список архивированных числовых записей.
+    - text (str): текущая текстовая запись для добавления в архив.
+    - number (int или float): текущая числовая запись для добавления в архив.
+    """
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.archive_text = []
+            cls._instance.archive_number = []
+        else:
+            cls._instance.archive_text.append(cls._instance.text)
+            cls._instance.archive_number.append(cls._instance.number)
+        return cls._instance
+
+    def __init__(self, text: str, number: Union[int, float]):
+        if text and isinstance(text, str):
+            self.text = text
+        else:
+            raise InvalidTextError(text)
+        
+        if isinstance(number, (int, float)) and number > 0:
+            self.number = number
+        else:
+            raise InvalidNumberError(number)
+
+    def __str__(self):
+        return f"Text is {self.text} and number is {self.number}. Also {self.archive_text} and {self.archive_number}"
+
+    def __repr__(self):
+        return f'Archive("{self.text}", {self.number})'
+
+
+class InvalidTextError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Invalid text: {self.value}. Text should be a non-empty string."
+
+
+class InvalidNumberError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Invalid number: {self.value}. Number should be a positive integer or float."
+
+
+# test 1
+archive_instance = Archive("Sample text", 42.5)
+print(archive_instance)
+'''Ожидаемый ответ:
+Text is Sample text and number is 42.5. Also [] and []
+'''
+
+# test 2
+invalid_archive_instance = Archive("", -5)
+print(invalid_archive_instance)
+'''Ожидаемый ответ:
+InvalidTextError: Invalid text: . Text should be a non-empty string.
+'''
+
+# test 3
+invalid_archive_instance = Archive("Sample text", -5)
+print(invalid_archive_instance)
+'''Ожидаемый ответ:
+__main__.InvalidNumberError: Invalid number: -5. Number should be a positive integer or float.
+
+'''
+
+
