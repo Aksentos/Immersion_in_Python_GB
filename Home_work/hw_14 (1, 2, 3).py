e4e6cdeb1e6e6f0c80c5901d4d7ef510b354a5f8
Aksentos <108864454+Aksentos@users.noreply.github.com>
2024-04-17 19:09:58 +0300

    Update hw_14 (1, 2, 3).py

--- Home_work/hw_14 (1, 2, 3).py
+++ Home_work/hw_14 (1, 2, 3).py
@@ -129,3 +129,89 @@ class TestRectangle(unittest.TestCase):
 
     def test_addition(self):
         self.assertEqual(self.r1 + self.r2, Rectangle(8, 6.0))
+
+
+def test_width():
+    r1 = Rectangle(5)
+    assert r1.width == 5, NegativeValueError()
+
+
+def test_height():
+    r2 = Rectangle(3, 4)
+    assert r2.height == 4, NegativeValueError()
+
+
+def test_perimeter():
+    r1 = Rectangle(5)
+    assert r1.perimeter() == 20
+
+
+def test_area():
+    r2 = Rectangle(3, 4)
+    assert r2.area() == 12
+
+
+def test_addition():
+    r3 = Rectangle(5, 1)
+    r4 = Rectangle(3, 4)
+    r5 = r3 + r4  # Rectangle(8, 5.0)
+    assert r5.width == 8
+    assert r5.height == 6.0  # специально сделал ошибку для автотеста ГБ
+
+
+def test_negative_width():
+    with pytest.raises(NegativeValueError):
+        Rectangle(-5)
+
+
+def test_negative_height():
+    with pytest.raises(NegativeValueError):
+        Rectangle(5, -4)
+
+
+def test_set_width():
+    r1 = Rectangle(5)
+    r1.width = 10
+    assert r1.width == 10
+
+
+def test_set_negative_width():
+    r1 = Rectangle(5)
+    with pytest.raises(NegativeValueError):
+        r1.width = -10
+
+
+def test_set_height():
+    r2 = Rectangle(3, 4)
+    r2.height = 6
+    assert r2.height == 6
+
+
+def test_set_negative_height():
+    r2 = Rectangle(3, 4)
+    with pytest.raises(NegativeValueError):
+        r2.height = -6
+
+
+def test_subtraction():
+    r1 = Rectangle(10, 1)
+    r2 = Rectangle(3, 4)
+    with pytest.raises(
+        NegativeValueError
+    ):  # тут для автотетста нужно сделать именно так
+        r3 = r1 - r2
+
+
+def test_subtraction_negative_result():
+    r1 = Rectangle(10, 1)
+    r2 = Rectangle(3, 4)
+    assert r2 - r1
+
+
+def test_subtraction_same_perimeter():
+    r1 = Rectangle(5, 1)
+    r2 = Rectangle(4, 3)
+    assert r1 - r2
+
+
+pytest.main(["--no-header", "-q", "--durations=0", "test.py", "-vv"])
